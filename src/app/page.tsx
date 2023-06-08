2d2fb7d40778cc1aeaa12b6d84c97a4a7cf192d6
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-09 00:55:25 +0100

    as

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import HotelCanvas from '@/components/360Hotel/HotelCanvas'
 import HotelCard from '@/components/HotelCard'
+import SearchBar from '@/components/SearchBar'
 import { useEffect, useState, useTransition } from 'react'
 const getHotels = async () => {
   const hotels_response = await fetch(`/api/hotels`)
@@ -19,6 +20,7 @@ export default function Home() {
     <main className='mx-6'>
       <div className='w-full h-80'>
         {/* <HotelCanvas /> */}
+        <SearchBar setHotels={setHotels} />
       </div>
       {isPending && <span className="loading loading-spinner loading-lg"></span>}
       <div className='grid grid-cols-3 grid-rows-3 gap-3'>
