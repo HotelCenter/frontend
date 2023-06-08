5c156c960e0a6dff3e3cd0253d57566e71467df2
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-08 02:56:10 +0100

    Loading component on button added

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useTransition } from 'react';
 import { signUpAction } from '../../actions/authActions';
+import Loading from '../loading';
 export default function Page() {
     const [isPending, startTransition] = useTransition()
 
@@ -62,7 +63,7 @@ export default function Page() {
                 </div>
                 <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
             </div>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" disabled={isPending}>{isPending ? <Loading /> : "Sign Up"}</button>
         </form>
 
     )
