3abbb1838af6a6315de614908f452cfc6bead5f5
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-08 02:55:44 +0100

    Loading component on button added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { loginAction } from "../../actions/authActions";
 import { useTransition } from "react";
+import Loading from "../loading";
 
 export default function Page() {
     const [isPending, startTransition] = useTransition()
@@ -19,7 +20,7 @@ export default function Page() {
                 <input type="password" id="password" name="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             </div>
 
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" disabled={isPending}>{isPending ? <Loading /> : "Login"}</button>
         </form>
 
     )
