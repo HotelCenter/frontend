20da3cd84900712cdf028eae9238d723139f6d45
Hossam madani <96637186+HossamElmadani@users.noreply.github.com>
2023-06-08 19:45:31 -0700

    I added from in search Bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -60,19 +60,22 @@ export default function SearchBar() {
     return (
         <div className="join left-[50%] max-w[1100px] absolute block" style={{transform: "translate(-50%,20px)", width: "calc(100% - 10px)"}}>
             <form className='m-0 p-0'>
-            <div className='flex box-border relative w-full'>
-            <div className='flex-auto ml-0'>
-                <div className='rounded-none bg-white self-center box-border w-full '>
-                    <div className='flex relative isolate  border border-indigo-500'>
-                        <div className=' items-center flex flex-grow w-full'>
-                            <div className='px-2 flex content-center relative whitespace-nowrap z-10'>
-                                <span className="w-5 align-top " aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.75 12h18.5c.69 0 1.25.56 1.25 1.25V18l.75-.75H.75l.75.75v-4.75c0-.69.56-1.25 1.25-1.25zm0-1.5A2.75 2.75 0 0 0 0 13.25V18c0 .414.336.75.75.75h22.5A.75.75 0 0 0 24 18v-4.75a2.75 2.75 0 0 0-2.75-2.75H2.75zM0 18v3a.75.75 0 0 0 1.5 0v-3A.75.75 0 0 0 0 18zm22.5 0v3a.75.75 0 0 0 1.5 0v-3a.75.75 0 0 0-1.5 0zm-.75-6.75V4.5a2.25 2.25 0 0 0-2.25-2.25h-15A2.25 2.25 0 0 0 2.25 4.5v6.75a.75.75 0 0 0 1.5 0V4.5a.75.75 0 0 1 .75-.75h15a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 0 1.5 0zm-13.25-3h7a.25.25 0 0 1 .25.25v2.75l.75-.75h-9l.75.75V8.5a.25.25 0 0 1 .25-.25zm0-1.5A1.75 1.75 0 0 0 6.75 8.5v2.75c0 .414.336.75.75.75h9a.75.75 0 0 0 .75-.75V8.5a1.75 1.75 0 0 0-1.75-1.75h-7z"></path></svg></span>
+                <div className='flex box-border relative w-full'>
+                    <div className='flex-auto ml-0'>
+                        <div className='rounded-none bg-white self-center box-border w-full p-2'>
+                            <div className='flex relative isolate  border border-indigo-500'>
+                                <div className=' items-center flex flex-grow w-full'>
+                                    <div className='px-2 flex content-center relative whitespace-nowrap z-10'>
+                                        <span className="w-5 align-top " aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.75 12h18.5c.69 0 1.25.56 1.25 1.25V18l.75-.75H.75l.75.75v-4.75c0-.69.56-1.25 1.25-1.25zm0-1.5A2.75 2.75 0 0 0 0 13.25V18c0 .414.336.75.75.75h22.5A.75.75 0 0 0 24 18v-4.75a2.75 2.75 0 0 0-2.75-2.75H2.75zM0 18v3a.75.75 0 0 0 1.5 0v-3A.75.75 0 0 0 0 18zm22.5 0v3a.75.75 0 0 0 1.5 0v-3a.75.75 0 0 0-1.5 0zm-.75-6.75V4.5a2.25 2.25 0 0 0-2.25-2.25h-15A2.25 2.25 0 0 0 2.25 4.5v6.75a.75.75 0 0 0 1.5 0V4.5a.75.75 0 0 1 .75-.75h15a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 0 1.5 0zm-13.25-3h7a.25.25 0 0 1 .25.25v2.75l.75-.75h-9l.75.75V8.5a.25.25 0 0 1 .25-.25zm0-1.5A1.75 1.75 0 0 0 6.75 8.5v2.75c0 .414.336.75.75.75h9a.75.75 0 0 0 .75-.75V8.5a1.75 1.75 0 0 0-1.75-1.75h-7z"></path></svg></span>
+                                    </div>
+                                        <input className="input join-item bg-base-100 border-l-none text-[14px] font-bold leading-5 width-full overflow-hidden overflow-ellipsis" placeholder="Destination..." />
+                                        <div className=' border border-black bottom-0 top-0 left-0 right-0 absolute rounded-lg '></div>
+                                </div>
                             </div>
-                                <input className="input join-item bg-base-100 border-l-none text-[14px] font-bold leading-5 width-full" placeholder="Destination..." />
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
 
             <Datepicker showShortcuts={true}
                 inputClassName='bg-base-200 border-none input join-item'
@@ -109,8 +112,7 @@ export default function SearchBar() {
                 <button className="btn join-item">Search</button>
             </div>
 
-            </div>
-            </form>
+            
             
         </div>
     )
