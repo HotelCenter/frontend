eb5e951274a9ecad26d35e92c2814d48df34a531
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-09 01:00:53 +0100

    .

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -3,11 +3,22 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import PaymentForm from "@/components/PaymentForm";
 import { useSearchParams } from "next/navigation";
+<<<<<<< HEAD
 import { useEffect, useRef, useState, useTransition } from "react";
 import ReserveRoomCard from "@/components/ReserveRoomCard";
 import Loading from "../loading";
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
 
+=======
+import { useEffect, useState, useTransition } from "react";
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+const options = {
+    currency: "eur",
+    amount: 1099,
+    mode: 'payment'
+
+};
+>>>>>>> 8a50b70 (Page reserve)
 const getRoom = async (roomId: Number) => {
     const room_response = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/rooms/${roomId}`);
     return room_response.json()
@@ -15,13 +26,18 @@ const getRoom = async (roomId: Number) => {
 export default function Page() {
     const params = useSearchParams()
     const [room, setRoom] = useState<RoomDataType>()
+<<<<<<< HEAD
     const [reservationDetails, setReservationDetails] = useState<ReservationDataType>(null!)
     const [isPending, startTransition] = useTransition()
     const modalRef = useRef<HTMLDialogElement>(null!)
+=======
+    const [isPending, startTransition] = useTransition()
+>>>>>>> 8a50b70 (Page reserve)
     useEffect(() => {
         startTransition(async () => setRoom(await getRoom(Number(params.get('room')))))
 
     }, [])
+<<<<<<< HEAD
 
     useEffect(() => {
         console.log(reservationDetails)
@@ -50,4 +66,17 @@ export default function Page() {
     </>
 
 
+=======
+    useEffect(() => {
+        console.log(room)
+
+    }, [room])
+    return <Elements stripe={stripePromise} options={{
+        mode: 'payment',
+        currency: 'usd',
+        amount: 55
+    }}>
+        <PaymentForm />
+    </Elements>
+>>>>>>> 8a50b70 (Page reserve)
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -49,11 +49,57 @@ export default function SearchBar({ setHotels }: { setHotels: Dispatch<SetStateA
                         </li>
                     </div>
                     <div className='grid grid-cols-2 items-center grid-rows-1'>
-                        <span>Rooms</span>
-                        <li>
-                            <input disabled={isPending} name="rooms" type="number" placeholder="Rooms" onChange={(e) => setRoomsCount(parseInt(e.target.value))} value={roomsCount} min={1} max={100} className="input input-bordered w-full max-w-xs" />
 
-                        </li>
+                        <div className='flex-auto ml-0 flex'>
+                            <div className='rounded-none bg-white self-center box-border w-full p-2'>
+                                <div className='flex relative isolate  '> {/* border border-indigo-500 */}
+                                    <div className=' items-center flex flex-grow w-full'>
+                                        <div className="dropdown w-full">
+                                            <label tabIndex={0} className="btn join-item">{adultsCount} Adults | {childrenCount} Children | {roomsCount} Rooms</label>
+                                            <ul tabIndex={0} className="space-y-2 dropdown-content menu p-5 shadow bg-base-300 rounded-box w-60">
+                                                <div className='grid grid-cols-2 items-center grid-rows-1'>
+                                                    <span>Adults</span>
+
+                                                    <li>
+                                                        <input name='adults' type="number" onChange={(e) => setAdultsCount(parseInt(e.target.value))} placeholder="Adults" value={adultsCount} min={1} max={100} className="input input-bordered w-full max-w-xs" />
+                                                    </li>
+                                                </div>
+                                                <div className='grid grid-cols-2 items-center grid-rows-1'>
+                                                    <span>Children</span>
+                                                    <li>
+                                                        <input name='children' type="number" onChange={(e) => setChildrenCount(parseInt(e.target.value))} placeholder="Children" value={childrenCount} min={0} max={10} className="input input-bordered w-full max-w-xs" />
+
+                                                    </li>
+                                                </div>
+                                                <div className='grid grid-cols-2 items-center grid-rows-1'>
+                                                    <span>Rooms</span>
+                                                    <li>
+                                                        <input name="rooms" type="number" placeholder="Rooms" onChange={(e) => setRoomsCount(parseInt(e.target.value))} value={roomsCount} min={1} max={100} className="input input-bordered w-full max-w-xs" />
+
+                                                    </li>
+                                                </div>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className='flex-auto ml-0'>
+                            <div className='rounded-none bg-white self-center box-border w-full p-2'>
+                                <div className='flex relative isolate  '> {/* border border-indigo-500 */}
+                                    <div className=' items-center flex flex-grow w-full'>
+                                        <div className="indicator">
+                                            <span className="indicator-item badge badge-secondary">new</span>
+                                            <button disabled={isPending} className="btn join-item" type='submit'>{isPending ? <ButtonLoading /> : "Search"}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
                     </div>
                 </ul>
             </div>
