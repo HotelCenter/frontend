0dc2b9fddfbd800dfdebff514e88bbc9fb7a1d7e
Hossam madani <96637186+HossamElmadani@users.noreply.github.com>
2023-06-10 13:59:17 -0700

    Changed HotelCard to another card for the raison that it was unresponsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import SearchBar from '@/components/SearchBar'
 import { useEffect, useState, useTransition } from 'react'
 const getHotels = async () => {
   const hotels_response = await fetch(`/api/hotels`)
-  return  hotels_response.json()
+  return hotels_response.json()
 }
 export default function Home() {
   const [isPending, setTransition] = useTransition()
@@ -18,27 +18,36 @@ export default function Home() {
   }, [])
   return (
     <main className='mx-6'>
-            {(!isPending && hotels.length !== 0) &&
+      {(!isPending && hotels.length !== 0) &&
 
-<>
-            <div className='w-full h-96'>
-
-
-        <HotelCanvas />
-
-      </div>
+        <>
+          <div className='w-full h-96'>
 
 
+            <HotelCanvas />
 
-            <SearchBar setHotels={setHotels} />
+          </div>
 
-</>
-}
+
+
+          <SearchBar setHotels={setHotels} />
+
+        </>
+      }
 
       {isPending && <span className="loading loading-spinner loading-lg"></span>}
-      <div className='grid grid-cols-3 grid-rows-3 gap-3'>
-        {(!isPending && hotels.length === 0) && <h1>No Hotel available</h1>}
-        {hotels.map((hotel, i) => <HotelCard hotel={hotel} key={i} />)}
+        {(!isPending && hotels.length === 0) &&
+          <div className="flex items-center justify-center h-screen">
+            <div className="bg-gray-200 p-6 rounded-lg shadow-lg">
+              <h1 className="text-2xl font-bold mb-4">No Hotel available</h1>
+              <p className="text-gray-600">We apologize, but there are no hotels available at the moment.</p>
+            </div>
+          </div>
+        }
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 grid-rows-3 gap-3 mb-20'>
+        {hotels.map((hotel, i) =>
+          <HotelCard hotel={hotel} key={i} />
+        )}
       </div>
     </main>
   )
