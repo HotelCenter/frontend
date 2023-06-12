97445e66a35cbae4b2e440fb77a49c3def71c820
Hossam madani <96637186+HossamElmadani@users.noreply.github.com>
2023-06-11 21:36:50 -0700

    fixing HotelCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
             </div>
           </div>
         }
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 grid-rows-3 gap-3 mb-20'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-3 gap-3 mb-20'>
         {hotels.map((hotel, i) =>
           <HotelCard hotel={hotel} key={i} />
         )}
--- src/components/HotelCard.tsx
+++ src/components/HotelCard.tsx
@@ -7,36 +7,36 @@ import Link from "next/link";
 
 
 export default function HotelCard({ hotel }: { hotel: HotelDataType }) {
-    const rateArr = getRates(hotel.rating)
-    return (
-<div className="max-w-xl mx-auto bg-white rounded-xl shadow-md overflow-hidden ">
-      <div className="xl:flex">
-        <div className="xl:flex-shrink-0">
-          <img className="h-48 xl:h-96 w-full object-cover xl:w-56" src={`/hotels_images/${hotel.image}`} alt={hotel.name} />
+  const rateArr = getRates(hotel.rating)
+  return (
+    <div className="max-w-xl mx-auto bg-white rounded-xl shadow-md overflow-hidden ">
+      <div className="2xl:flex">
+        <div className="2xl:flex-shrink-0">
+          <img className="h-48 2xl:h-96 w-full object-cover 2xl:w-56" src={`/hotels_images/${hotel.image}`} alt={hotel.name} />
         </div>
-        <div className="p-8 flex flex-col justify-between">
+        <div className="flex m-1 3xl:m-4 flex-col justify-between">
           <div>
             <h2 className="uppercase tracking-wide text-md text-indigo-500 font-semibold">{hotel.name}</h2>
             {/* <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{hotel.name}</a> */}
             <p className="mt-2 text-gray-500">{hotel.description}</p>
             <div className="mt-4 flex items-center">
-            {rateArr.map((faIcon, i) => <FontAwesomeIcon icon={faIcon} key={i} className="text-yellow-200" />)}
-              <span className="text-gray-700 ml-2">4.5</span>
+              {rateArr.map((faIcon, i) => <FontAwesomeIcon icon={faIcon} key={i} className="text-yellow-200" />)}
+              <span className="text-gray-700 ml-2">{hotel.rating}</span>
             </div>
           </div>
-          <div className="mt-6">
-          <Link href={`/rooms/${hotel.slug}`}>
-            <button className="btn btn-primary">Check</button>             
-          </Link>          
+          <div>
+            {/* <div className="cursor-pointer mt-2 3xl:mt-4 text-sm 3xl:text-md flex justify-center text-blue-500">
+              <FontAwesomeIcon icon={faLocationDot} /> <span> {hotel.city} , {hotel.country}</span>
+            </div> */}
           </div>
-        </div>
-      </div>
-      <div className="mt-4 flex items-center justify-center text-blue-500">
-        <div className="flex cursor-pointer ">
-          <FontAwesomeIcon icon={faLocationDot} /> <span> {hotel.city} , {hotel.country}</span>
+          <div className="mt-2 3xl:mt-6">
+            <Link href={`/rooms/${hotel.slug}`}>
+              <button className="btn btn-primary">Check</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
 
-    )
+  )
 }
