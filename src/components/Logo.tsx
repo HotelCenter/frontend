da9a532179aa91b7c34b236f93167994d76f0910
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-13 00:50:09 +0100

    changed logo,added Logo component for resung and add navbar to admin

---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Logo() {
+    return <>
+        <Image width={250} height={100} src="/hotelcenter.svg" alt="HotelCenter Logo" />
+    </>
+}
