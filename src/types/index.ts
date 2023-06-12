b0a7f0b5abbda1555888aae19f3bee77b77c0b11
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-12 22:03:53 +0100

    is_admin propery added

--- src/types/index.ts
+++ src/types/index.ts
@@ -51,16 +51,9 @@ type UserDataType = {
     city: string;
     phone_number: string;
     code_postal: string;
-<<<<<<< HEAD
-}
-
-type MiddleRoutes = {
-    key: Array<any> | null | object
-=======
     is_admin: boolean
 }
 
 type MiddleRoutes = {
     [key: string]: object | Array<number | string> | null
->>>>>>> admin
 }
