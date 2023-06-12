69e937c8cebedf61322ac2e9668d6f5f1ba26e7f
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-13 00:50:09 +0100

    changed logo,added Logo component for resung and add navbar to admin

--- src/components/navbars/auth.tsx
+++ src/components/navbars/auth.tsx
@@ -1,14 +1,12 @@
-import { logoutAction } from "@/actions/authActions"
-import Image from "next/image"
 import Link from "next/link"
+import Logo from "../Logo"
 
 export default function Navbar() {
 
     return (<nav className="bg-white dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
             <Link href="/" className="flex items-center">
-                <Image width={100} height={100} src="/hotelcenter.svg" alt="HotelCenter Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HotelCenter</span>
+                <Logo />
             </Link>
             <div className="flex md:order-2">
                 <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
