7e55476dcececa125471fe87210f387dd4459b77
Muhssin Elnamli <ennemli.moh55@gmail.com>
2023-06-12 21:37:14 +0100

    Made footer fixed at bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" >
-      <body className={`${inter.className} min-h-screen`} >
+      <body className={`${inter.className} min-h-screen flex flex-col`} >
         {children}
         <Footer />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
