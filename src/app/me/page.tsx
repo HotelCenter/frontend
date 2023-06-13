b719acfdbb6dabff102490e85ce289ffc1e77852
Hossam madani <96637186+HossamElmadani@users.noreply.github.com>
2023-06-13 02:01:26 -0700

    profile

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -20,80 +20,48 @@ const getUser = async () => {
 export default async function Page() {
   const user: UserDataType = await getUser()
   return (
-    <div className="bg-gray-100 py-6">
-      <div className="max-w-xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-        <div className="px-6 py-4">
-          <div className="text-2xl font-bold mb-2">User Profile</div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              ID
-            </label>
-            <p className="text-gray-900 text-base">{user.id}</p>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-slate-200 to-slate-600">
+      <div className="bg-white rounded-lg shadow-md w-96 p-8">
+        <div className="flex items-center justify-center">
+          <img
+            src="profile.jpg"
+            alt="Avatar"
+            className="w-24 h-24 rounded-full border-4 border-white"
+          />
+        </div>
+        <div className="mt-6 text-center">
+          <h2 className="text-2xl font-bold text-slate-800">
+            {user.first_name} {user.last_name}
+          </h2>
+          <p className="mt-2 text-gray-600">{user.email}</p>
+          <p className="mt-2 text-bold text-info">
+            {user.is_admin ? 'Admin' : 'User'}
+          </p>
+        </div>
+        <div className="mt-8">
+          <div className="flex justify-between text-gray-700">
+            <span>Address:</span>
+            <span>{user.address}</span>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Name
-            </label>
-            <p className="text-gray-900 text-base">
-              {user.first_name} {user.last_name}
-            </p>
+          <div className="flex justify-between text-gray-700">
+            <span>Country:</span>
+            <span>{user.country}</span>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email
-            </label>
-            <p className="text-gray-900 text-base">{user.email}</p>
+          <div className="flex justify-between text-gray-700">
+            <span>City:</span>
+            <span>{user.city}</span>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email Verified
-            </label>
-            <p className="text-gray-900 text-base">
-              {user.email_verified_at ? 'Yes' : 'No'}
-            </p>
+          <div className="flex justify-between text-gray-700">
+            <span>Phone Number:</span>
+            <span>{user.phone_number}</span>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Address
-            </label>
-            <p className="text-gray-900 text-base">{user.address}</p>
+          <div className="flex justify-between text-gray-700">
+            <span>Postal Code:</span>
+            <span>{user.code_postal}</span>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Country
-            </label>
-            <p className="text-gray-900 text-base">{user.country}</p>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              City
-            </label>
-            <p className="text-gray-900 text-base">{user.city}</p>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Phone Number
-            </label>
-            <p className="text-gray-900 text-base">{user.phone_number}</p>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Postal Code
-            </label>
-            <p className="text-gray-900 text-base">{user.code_postal}</p>
-          </div>
-          {user.is_admin &&
-            <div className="mb-4">
-              <Link href={'/admin'}>
-                <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                  Admin Dashboard
-                </button>
-              </Link>
-            </div>
-          }
         </div>
       </div>
-    </div >
+    </div>
   );
 
 }
